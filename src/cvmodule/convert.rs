587b77f74ff24c787b1a9bf01745531fec1307c6
tech-kind <j149335.oku@gmail.com>
2023-05-10 15:24:27 +0900

    add grayscale and posterization

--- src/cvmodule/convert.rs
+++ src/cvmodule/convert.rs
@@ -8,6 +8,7 @@ pub enum Channel {
     B
 }
 
+#[allow(dead_code)]
 pub fn get_channel_image(rgb: ImageBuffer<Rgb<u8>, Vec<u8>>, channel: Channel)
     -> ImageBuffer<Luma<u8>, Vec<u8>> {
     let width = rgb.width();
@@ -27,5 +28,41 @@ pub fn get_channel_image(rgb: ImageBuffer<Rgb<u8>, Vec<u8>>, channel: Channel)
             let val = rgb.get_pixel(*x, *y);
             pixel[0] = val[color];
         });
+    img
+}
+
+#[allow(dead_code)]
+pub fn gray_scale(rgb: ImageBuffer<Rgb<u8>, Vec<u8>>)
+    -> ImageBuffer<Luma<u8>, Vec<u8>> {
+    let width = rgb.width();
+    let height = rgb.height();
+
+    let mut img = GrayImage::new(width, height);
+    img.enumerate_pixels_mut()
+    .collect::<Vec<(u32, u32, &mut Luma<u8>)>>()
+    .par_iter_mut()
+    .for_each(|(x, y, pixel)| {
+        let val = rgb.get_pixel(*x, *y);
+        pixel[0] = (0.2126 * val[0] as f32 + 0.7152 * val[1] as f32 + 0.0722 * val[2] as f32) as u8;
+    });
+    img
+}
+
+#[allow(dead_code)]
+pub fn posterization(gray: ImageBuffer<Luma<u8>, Vec<u8>>, quantization_number: u8)
+    -> ImageBuffer<Luma<u8>, Vec<u8>> {
+    let width = gray.width();
+    let height = gray.height();
+
+    let color = 255 / quantization_number;
+
+    let mut img = GrayImage::new(width, height);
+    img.enumerate_pixels_mut()
+    .collect::<Vec<(u32, u32, &mut Luma<u8>)>>()
+    .par_iter_mut()
+    .for_each(|(x, y, pixel)| {
+        let val = gray.get_pixel(*x, *y);
+        pixel[0] = ((val[0] / color) * color + (color / 2)) as u8;
+    });
     img
 }
